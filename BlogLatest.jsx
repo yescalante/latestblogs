782fb45ab317fa011e3b59b2dc873d68bca7fa9d
Yazmine Escalante <77589798+yescalante@users.noreply.github.com>
2021-04-30 09:42:02 -0700

    Add files via upload

---
+++ BlogLatest.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { blogLatestProps } from "../blogs/blogProps";
+import * as blogService from "../../services/blogService";
+import { Grid, Card, Button, List } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+import { BarLoader } from "react-spinners";
+import InfiniteScroll from "react-infinite-scroll-component";
+import BlogSingle from "./BlogSingle";
+import debug from "sabio-debug";
+
+const _logger = debug.extend("Latestblogs");
+
+class BlogLatest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mappedBlogs: [],
+      pageIndex: 0,
+      pageSize: 10,
+      hasMore: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getBlogs();
+  }
+
+  getBlogs = () => {
+    blogService
+      .getAll(this.state.pageIndex, this.state.pageSize)
+      .then(this.getAllBlogsSuccess)
+      .catch(this.onGetAllError);
+  };
+
+  getAllBlogsSuccess = (res) => {
+    let blogs = res.item.pagedItems.filter((blog) => blog.statusType.Id !== 7);
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        mappedBlogs: [...prevState.mappedBlogs, blogs.map(this.mapSingleBlog)],
+        pageIndex: this.state.pageIndex + 1,
+      };
+    });
+  };
+
+  mapSingleBlog = (blog) => {
+    return (
+      <BlogSingle blog={blog} readBlog={this.readBlogClicked} key={blog.id} />
+    );
+  };
+
+  onGetAllError = (response) => {
+    _logger({ error: response });
+  };
+
+  readBlogClicked = (blog) => {
+    this.props.history.push(`/blogs/${blog.id}`, blog);
+  };
+
+  goToBlogs = () => {
+    this.props.history.push("/blogs");
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Grid container spacing={4}>
+          <Grid item xs={12} xl={6}>
+            <Card className="card-box mb-4">
+              <div
+                className="card-header pr-1"
+                style={{ backgroundColor: "#5E3267" }}
+              >
+                <div className="card-header--title">
+                  <small
+                    className="d-block text-uppercase mt-1"
+                    style={{ color: "white" }}
+                  >
+                    Blogs
+                  </small>
+                  <b style={{ color: "white" }}>Latest Blogs</b>
+                </div>
+                <div className="card-header--actions">
+                  <Button
+                    size="small"
+                    variant="contained"
+                    color="secondary"
+                    className="mr-3 bg-strong-bliss text-white"
+                    onClick={this.goToBlogs}
+                  >
+                    Read More Blogs
+                  </Button>
+                </div>
+              </div>
+              <div id="scrollableDivOne" className="scroll-area-lg">
+                <List>
+                  <InfiniteScroll
+                    dataLength={this.state.mappedBlogs.length}
+                    next={this.getBlogs}
+                    hasMore={this.state.hasMore}
+                    scrollThreshold={0.8}
+                    scrollableTarget="scrollableDivOne"
+                    loader={
+                      <div className="d-flex align-items-center justify-content-center py-3">
+                        <BarLoader color={"var(--primary)"} loading={true} />
+                      </div>
+                    }
+                    endMessage={
+                      <div className="mb-2">
+                        <MuiAlert severity="success">
+                          This is the end of the list!
+                        </MuiAlert>
+                      </div>
+                    }
+                  >
+                    {this.state.mappedBlogs}
+                  </InfiniteScroll>
+                </List>
+              </div>
+              <div
+                className="card-footer d-flex justify-content-between"
+                style={{ backgroundColor: "#5E3267" }}
+              ></div>
+            </Card>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+BlogLatest.propTypes = blogLatestProps;
+
+export default BlogLatest;
---
+++ BlogSingle.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { singleBlogProps } from "../blogs/blogProps";
+import { Card, Button, List, ListItem } from "@material-ui/core";
+
+const BlogSingle = (props) => {
+  const readBlogClicked = () => {
+    props.readBlog(props.blog);
+  };
+
+  return (
+    <div className="scroll-area-sm shadow-overflow">
+      <React.Fragment>
+        <List>
+          <ListItem className="py-4">
+            <div className="d-flex flex-column flex-sm-row align-items-start">
+              <div>
+                <Card className="card-transparent mb-3 mb-sm-0">
+                  <a className="card-img-wrapper rounded">
+                    <img
+                      alt="..."
+                      className="card-img-top rounded"
+                      src={props.blog.imageUrl}
+                      style={{ width: 180 }}
+                    />
+                  </a>
+                </Card>
+              </div>
+              <div className="pl-0 pl-sm-4">
+                <div className="mb-1">
+                  <a
+                    className="font-size-lg"
+                    href="#/"
+                    onClick={readBlogClicked}
+                  >
+                    {props.blog.title}
+                  </a>
+                </div>
+                <div>
+                  <p className="mb-0 mt-2 text-black-50">
+                    {props.blog.subject}
+                  </p>
+                </div>
+                <div className="d-flex align-items-center">
+                  <Button
+                    size="small"
+                    variant="contained"
+                    color="primary"
+                    className="m-3 bg-amy-crisp text-white"
+                    onClick={readBlogClicked}
+                  >
+                    Read Blog
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </ListItem>
+        </List>
+      </React.Fragment>
+    </div>
+  );
+};
+
+BlogSingle.propTypes = singleBlogProps;
+
+export default BlogSingle;
